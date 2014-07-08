c224060688add0d06b66804ce8d3664f42ba1224
David Bayon <dbayon82@googlemail.com>
2014-07-08 09:58:02 +0100

    Starting van and garage over

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,13 +1,3 @@
 class Garage
-	def initialize
-		@bikes = []
-	end
-
-	def bikes
-		@bikes
-	end
-
-	def receive(bike)
-		bikes << bike
-	end
+	
 end
--- lib/van.rb
+++ lib/van.rb
@@ -1,32 +1,4 @@
 class Van
 
-	def initialize
-		@bikes = []
-	end
-
-	def bikes
-		@bikes
-	end
-
-	def collect_bike(bike, docking_station)
-		bikes << bike
-		docking_station.undock(bike)
-	end
-
-	def deliver_bike(bike, docking_station)
-		bikes.delete(bike)
-		docking_station.dock(bike)
-	end
-
-	def collect_for_repair(bikes_to_collect, docking_station)
-		bikes_to_collect.each do |bike|
-			collect_bike(bike, docking_station) if bike.broken?
-		end
-	end
-
-	def return_repaired(bikes_to_collect, docking_station)
-		bikes_to_collect.each do |bike|
-			deliver_bike(bike, docking_station) if !bike.broken?
-		end
-	end
+	
 end
