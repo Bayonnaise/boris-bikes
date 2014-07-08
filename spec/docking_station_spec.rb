1c0d748230abe0bab7b58d16b603de97894288d0
David Bayon <dbayon82@googlemail.com>
2014-07-08 09:46:05 +0100

    Station capacities added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,11 @@ require 'bike'
 describe DockingStation do
 	let(:nagamura) { Bike.new }
 	let(:old_street) { DockingStation.new }
+	let(:station) { DockingStation.new(:capacity => 20) }
+
+	def fill_station(station)
+		20.times { station.dock(Bike.new) }
+	end
 
 	it 'has no bikes when created' do
 		expect(old_street.bikes.count).to eq 0
@@ -20,6 +25,15 @@ describe DockingStation do
 		expect(old_street.bikes).not_to eq [nagamura]
 	end
 
+	it "should know when it's full" do
+		expect(station).not_to be_full
+		fill_station(station)
+		expect(station).to be_full
+	end
 	
+	it 'should not accept a bike if the station is full' do
+		fill_station(station)
+		expect(lambda { station.dock(nagamura) }).to raise_error(RuntimeError)
+	end
 
 end
