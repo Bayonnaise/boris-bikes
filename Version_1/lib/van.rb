5c0e02b8bfc137f30ff19f677cebfb56e7fda433
David Bayon <dbayon82@googlemail.com>
2014-07-09 15:31:56 +0100

    final working Version 1

--- Version_1/lib/van.rb
+++ Version_1/lib/van.rb
@@ -8,43 +8,34 @@ class Van
 		self.capacity = options.fetch(:capacity, capacity)
 	end
 
-	def takes_broken_bikes_from(station)
-		station.unavailable_bikes.each do |bike|
-			station.release(bike) if !self.full?
+	def takes_broken_bikes_from(from_station)
+		takes(from_station.unavailable_bikes, from_station)
+	end
+
+	def takes_fixed_bikes_from(from_station)
+		takes(from_station.available_bikes, from_station)
+	end
+
+	def delivers_fixed_bikes_to(to_station)
+		delivers(self.available_bikes, to_station)
+	end
+
+	def delivers_broken_bikes_to(to_station)
+		delivers(self.unavailable_bikes, to_station)
+	end
+
+	def takes(bikes, from_station)
+		bikes.each do |bike|
+			from_station.release(bike) if !self.full?
 			self.dock(bike)
-		end	
-	end
-
-	def delivers_broken_bikes_to(garage)
-		self.unavailable_bikes.each do |bike|
-			self.release(bike) if !garage.full?
-			garage.dock(bike)
 		end
 	end
 
-	def takes_fixed_bikes_from(garage)
-		garage.available_bikes.each do |bike|
-			garage.release(bike) if !self.full?
-			self.dock(bike)
+	def delivers(bikes, to_station)
+		bikes.each do |bike|
+			self.release(bike) if !to_station.full?
+			to_station.dock(bike)
 		end
 	end
-
-	def delivers_fixed_bikes_to(station)
-		self.available_bikes.each do |bike|
-			self.release(bike) if !station.full?
-			station.dock(bike)
-		end
-	end
-
 end
-
-
-
-# def delivers_fixed_bikes_to(x, y, a, )
-# 	(y).(a).each do |bike|
-# 		(y).release(bike) if !(x).full?
-# 		(x).dock(bike)
-# 	end
-# end
-
 
