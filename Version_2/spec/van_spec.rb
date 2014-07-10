115fc6975f8c24fa6e6cfb4b9fbaca5ecf0d87c0
David Bayon <dbayon82@googlemail.com>
2014-07-10 10:22:01 +0100

    Final commit on V2

--- Version_2/spec/van_spec.rb
+++ Version_2/spec/van_spec.rb
@@ -4,8 +4,8 @@ describe Van do
 
 	let(:van) { Van.new }
 	let(:bike) { double :bike }
-	# let(:broken_bike) { Bike.new.break! }
-	let(:docking_station) { double :docking_station, release: :bike, unavailable_bikes: [bike] }
+	let(:bike2) { double :bike, broken: true }
+ 	let(:docking_station) { double :docking_station, release: :bike, unavailable_bikes: [bike] }
 	let(:garage) { double :garage }
 
 	it_behaves_like 'a bike container'
@@ -14,16 +14,31 @@ describe Van do
 		expect(van.bikes.count).to eq 0
 	end
 
-	it 'takes broken bikes from station' do
+	it 'takes bikes' do
+		expect(van).to receive(:full?).twice
+		expect(docking_station).to receive(:release).with(bike)
+		expect(van).to receive(:dock).with(bike)
+		expect(docking_station).to receive(:release).with(bike2)
+		expect(van).to receive(:dock).with(bike2)
+		van.takes([bike, bike2], docking_station)
+	end
+
+	it 'delivers bikes' do
+		expect(docking_station).to receive(:full?).twice
+		expect(van).to receive(:release).with(bike)
 		expect(docking_station).to receive(:dock).with(bike)
-		docking_station.dock(bike)
+		expect(van).to receive(:release).with(bike2)
+		expect(docking_station).to receive(:dock).with(bike2)
+		van.delivers([bike, bike2], docking_station)
+	end
+
+	it 'takes broken bikes from station' do
 		expect(van).to receive(:takes).with([bike], docking_station)
 		van.takes_broken_bikes_from(docking_station)
 	end
 
 	xit 'delivers broken bikes to garage' do
 		van.dock(bike)
-		van.dock(broken_bike)
 
 		van.delivers_broken_bikes_to(garage)
 
